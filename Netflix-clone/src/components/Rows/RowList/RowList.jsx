e23220683951fb52b5afa22ed7d4db13edff29bd
Feven-Zeray <fevenzeray2028@gmail.com>
2025-02-18 16:47:18 -0600

    env deleted

---
+++ Netflix-clone/src/components/Rows/RowList/RowList.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const RowList = () => {
+  return (
+    <>
+    <Row />
+    <Row />
+    <Row />
+    <Row />
+    <Row />
+
+    </>
+  )
+}
+
+export default RowList
